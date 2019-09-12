3e84189fda54e710040e69685bdac7f6ae4311c4
novayo <f14051172@gs.ncku.edu.tw>
2019-09-12 16:06:37 +0800

    Draw mario character

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,37 @@
-import SpriteSheet from './SpriteSheet.js';
-import {loadImage, loadLevel} from './loaders.js'; 
-
-function drawBackground(background, context, sprites){
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x=x1; x<x2; x++){
-            for (let y=y1; y<y2; y++){
-                sprites.drawTile(background.tile, context, x, y); // 放在(45, 62)的位置
-            }
-        }
-    });
-}
+import Compositor from './Compositor.js'
+import {loadLevel} from './loaders.js'; 
+import {loadMarioSprite, loadBackgroundSprites} from './sprites.js'; 
+import {createBackgroundLayer, createSpriteLayer} from './layers.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-loadImage('/img/tiles.png')
-.then(image => {
-    const sprites = new SpriteSheet(image, 16, 16); // 拿圖片，要拿16*16
-    sprites.define('ground', 0, 0); // 從圖片中的(0, 0)開始拿
-    sprites.define('sky', 3, 23);
-    
-    loadLevel('1-1')
-    .then(level => {
-        level.backgrounds.forEach(background => {
-            drawBackground(background, context, sprites); 
-        });
-    });
+Promise.all([
+    loadMarioSprite(),
+    // 同時讀取完 兩function後
+    loadBackgroundSprites(), // 回傳值給sprites
+    loadLevel('1-1'),        // 回傳值給level
+])
+.then(([marioSprite, backgroundSprites, level]) => {
+    // 確保兩個回傳值都拿到之後，才會做then內的東西
+    // 也就是確保兩個值都拿到之後才會畫出來
+    const pos = {
+        x: 64,
+        y: 64,
+    }
+
+    const comp = new Compositor();
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    comp.layers.push(backgroundLayer);
+    comp.layers.push(spriteLayer);
+
+    function update(){
+        comp.draw(context);
+        pos.x += 2;
+        pos.y += 2;
+        requestAnimationFrame(update); // 告訴網頁每當要刷新頁面的時候，都要跑一次指定的函數
+    }
+
+    update();
 });
