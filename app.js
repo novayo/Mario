5d2045a168c37c5df428f1ebadd5f0cc52114758
novayo <f14051172@gs.ncku.edu.tw>
2019-09-12 17:01:35 +0800

    Express V2

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+const server = require('http').createServer(app);
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = 5000; // you can use any port
-app.listen(port);
-console.log('server on ' + port);
+
+const SERVER_PORT = process.env.PORT || 2000;
+app.set('port', SERVER_PORT);
+
+// Start Express server
+server.listen(app.get('port'), () => {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
